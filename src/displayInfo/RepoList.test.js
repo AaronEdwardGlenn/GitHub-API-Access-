f78fac2180683133c0d177dc9787fbf51567d848
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-22 21:25:19 -0800

    added RepoList.js

--- src/displayInfo/RepoList.test.js
+++ src/displayInfo/RepoList.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import RepoList from './RepoList';
 import { Provider } from 'react-redux';
-import store from '../../store';
+import store from '../store';
 
 describe('RepoList component', () => {
   it('matches snapshot', () => {
--- src/displayInfo/User.test.js
+++ src/displayInfo/User.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import User from './User';
 import { Provider } from 'react-redux';
-import store from '../../store';
+import store from '../store';
 
 describe('User component', () => {
   it('canmatch the snap', () => {
