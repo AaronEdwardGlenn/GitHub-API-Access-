0765e0bca1dddc0cedef0423e586bf9a9b5a1c0c
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-22 20:13:57 -0800

    added GitUserACT test

---
+++ src/dater/getGitUserACT.test.js
@@ -0,0 +1,23 @@
+import { GET_USER_FROM_API, getUserFromAPI, GET_USER } from './getGitUserACT';
+
+jest.mock('../services/__mocks__/githubApi');
+
+describe('ACTions', () => {
+  it('getUserFromAPI functions properly', () => {
+    const dispatch = jest.fn();
+    const action = getUserFromAPI('user');
+    return action(dispatch)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledWith({
+          type: GET_USER_FROM_API
+        });
+        expect(dispatch).toHaveBeenCalledWith({
+          type: GET_USER,
+          payload: {
+            id: 1234,
+            name: 'CalvinCoolidge'
+          }
+        });
+      });
+  });
+});
